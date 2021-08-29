a7335b5b5c04b931fb25e9cf71cc40422d8b1c95
cj <kalicharan.hbs@gmail.com>
2021-08-29 12:19:29 +0530

    Install Powerlevel10k

--- packages.sh
+++ packages.sh
@@ -2,3 +2,4 @@ sudo pacman -Syyu terminus-font exa zsh neofetch powerline-fonts curl
 
 #Installing ohmyzsh 
 sh -c "$(curl -fsSL http://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 http://github.com/romkatv/powerlevel10k.git /home/cj/.oh-my-zsh/themes/powerlevel10k
