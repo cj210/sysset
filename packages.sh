2629a24f6368ea572a544ea8bc2f3f9076085967
cj <kalicharan.hbs@gmail.com>
2021-08-29 12:07:31 +0530

    Install ohmyzsh set .zshrc

--- packages.sh
+++ packages.sh
@@ -1,1 +1,4 @@
 sudo pacman -Syyu terminus-font exa zsh neofetch powerline-fonts curl
+
+#Installing ohmyzsh 
+sh -c "$(curl -fsSL http://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
